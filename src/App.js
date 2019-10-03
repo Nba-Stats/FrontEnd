f641304b31db0d512c4bd9a6e50fdd894362646c
Connor Claxton <czclaxton@gmail.com>
2019-10-03 16:50:10 -0500

    boiler plate setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
